ed383a6ea83a719e024a938d8485955f05a92b5d
Eastascend <947747330@qq.com>
2017-05-01 12:40:12 +0800

    Signed-off-by: Eastascend <947747330@qq.com>

--- app/src/main/java/kevin/com/CatTalk/Ui/Activity/SplashActivity.java
+++ app/src/main/java/kevin/com/CatTalk/Ui/Activity/SplashActivity.java
@@ -14,7 +14,7 @@ public class SplashActivity extends AppCompatActivity {
 
     private static final int GO_HOME = 0;//去主页
     private static final int GO_LOGIN = 1;//去登录页
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
